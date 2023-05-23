efdf5f4c41d6d554b49864b6e639b14a985e4efe
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-05-23 11:36:19 -0500

    update

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,33 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    //Tutorial
-    bool swapScene = false;
-    public float swapSceneCurrent = 0f;
-    public float swapSceneMax = 3f;
-    public float swapSceneFactor = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     if(swapScene)
-    //     {
-
-    //         SceneManager.LoadScene("Game");
-            
-    //     }
-    // }
+    [SerializeField] private string sceneName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Game");
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
